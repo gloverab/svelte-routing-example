91b827f3bbb5ebd51a086884a299d9511e4e29e2
Alex Glover <gloverab@Alexs-MacBook-Pro.local>
2021-04-07 11:56:55 -0400

    contact page

--- src/Contact.svelte
+++ src/Contact.svelte
@@ -1,6 +1,7 @@
 <h1>I'm a contact page</h1>
 
 <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24"><path fill='#000' d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg>
-<h3>Follow me on <a href='https://github.com/gloverab' target='blank'>Github</a></h3>
-<h3>Follow me on <a href='https://twitter.com/ellimistmusic' target='blank'>Twitter</a></h3>
-<h3>Check out my <a href='https://soundcloud.com/ellimistmusic/florish' target='blank'>Music</a></h3>
+<h3><a href='https://github.com/gloverab/svelte-routing-example' target='blank'>View this project on Github</a></h3>
+<h3><a href='https://github.com/gloverab' target='blank'>Follow me on Github</a></h3>
+<h3><a href='https://twitter.com/ellimistmusic' target='blank'>Follow me on Twitter</a></h3>
+<h3><a href='https://soundcloud.com/ellimistmusic/florish' target='blank'>Check out my Music</a></h3>
